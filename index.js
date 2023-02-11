701d3411a8d2fe8bd058d3b5fc48f82a2e2f38ab
Elizabeth Salako <waleade602@gmail.com>
2023-02-11 11:09:09 -0600

    console

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function mul(a, b) {
 
 // the function mul is called while passing two arguments.
 mul(3, 5);
+
+
+let firstName = "Elizabeth";
+let lastName = "Salako";
+let track = "Backend Web Development";
+let skills = "Research, Critical-thinking, Fashion designing and Painting";
+let favColor = "Black and Blue";
+let countryIndp = "1960";
+
+console.log("My name is " + firstName, lastName + "." + " I am a student at Stutern in the", track, "track." + "\n" + "My skills are: " + skills + ". I love the colors: " + favColor + "." + "\n" + "I am from Nigeria, and the Country got its' Independence in the year " + countryIndp + ".");
