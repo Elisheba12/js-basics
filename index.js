2b60eddd99b8adc8507db21a104d1677c81f10f3
Elizabeth Salako <waleade602@gmail.com>
2023-02-11 10:36:26 -0600

    mul

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// function "mul" takes two arguments represented as parameters "a" and "b", and logs the multiplication to the console.
 function mul(a, b) {
     console.log(a * b)
 }
 
+// the function mul is called while passing two arguments.
 mul(3, 5)
